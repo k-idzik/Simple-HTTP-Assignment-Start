9527944817c3c9e206cae9e0ca63792e61b2bbe4
Kevin <kdi4293@rit.edu>
2017-09-07 12:27:05 -0400

    Added initial files to project

---
+++ src/server.js
@@ -0,0 +1,4 @@
+const http = require('http');
+const htmlHandler = require('./htmlResponses.js');
+const textHandler = require('./textResponses.js');
+const jsonHandler = require('./jsonResponses.js');
